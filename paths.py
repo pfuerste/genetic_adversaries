14dd721deea1dab87122036135e866745628b2f5
Philip <philip.fuerste@gmail.com>
2019-06-26 16:05:26 +0200

    imports

---
+++ paths.py
@@ -0,0 +1,48 @@
+import os
+import random
+
+import numpy as np
+from keras.utils import to_categorical
+
+
+def get_data_path():
+    try:
+        path = './data/'
+    except FileNotFoundError:
+        path = '../content/drive/My Drive/data'
+    return path
+
+
+def get_dummy_path():
+    try:
+        path = './dummydata/'
+    except FileNotFoundError:
+        path = '../content/drive/My Drive/dummydata'
+    return path
+
+
+# Input: Folder Path
+def get_labels(path=get_data_path()):
+    # labels = os.listdir(path)
+    labels = [name for name in os.listdir(path) if os.path.isdir(os.path.join(path, name))]
+    if 'mfcc_vectors' in labels:
+        labels.remove('mfcc_vectors')
+    if 'mfcc_vectors_big' in labels:
+        labels.remove('mfcc_vectors_big')
+    if 'spec_vectors' in labels:
+        labels.remove('spec_vectors')
+    if '_background_noise_' in labels:
+        labels.remove('_background_noise_')
+    label_indices = np.arange(0, len(labels))
+    return labels, label_indices, to_categorical(label_indices)
+
+
+def pick_random_sample(path=get_data_path(), input_size=32044):
+    labels = get_labels(path)[0]
+    label = random.choice(labels)
+    rnd_label_path = os.path.join(path, label)
+    rnd_sample = random.choice([x for x in os.listdir(rnd_label_path)
+                                if os.path.isfile(os.path.join(rnd_label_path, x))
+                                and os.path.getsize(os.path.join(rnd_label_path, x)) == input_size])
+    rnd_sample_path = os.path.join(path, label, rnd_sample)
+    return rnd_sample_path
