5051e3465997905d34fa28ebf4e4c4e622eacfcf
Philip <philip.fuerste@gmail.com>
2019-06-29 12:25:07 +0200

    added model 4

--- model.py
+++ model.py
@@ -45,7 +45,7 @@ class Model:
             model.add(Dense(self.num_classes, activation='softmax'))
 
         elif type == 3:
-            print('Experimental Model chosen.')
+            print('Experimental Model (3) chosen.')
             model = Sequential()
             model.add(Conv2D(64, kernel_size=(8, 20), activation='relu', input_shape=input_shape))
             model.add(Dropout(0.25))
@@ -53,6 +53,21 @@ class Model:
             model.add(Conv2D(64, kernel_size=(4, 10), activation='relu'))
             model.add(Flatten())
             model.add(Dense(32, activation='relu'))
+            model.add(Dense(128, activation='relu'))
+            model.add(Dropout(0.4))
+            model.add(Dense(self.num_classes, activation='softmax'))
+
+        elif type == 4:
+            print('Experimental Model (3) chosen.')
+            model = Sequential()
+            model.add(Conv2D(64, kernel_size=(8, 20), activation='relu', input_shape=input_shape))
+            model.add(Dropout(0.25))
+            model.add(MaxPooling2D(pool_size=(3, 1)))
+            model.add(Conv2D(64, kernel_size=(4, 10), activation='relu'))
+            model.add(Conv2D(32, kernel_size=(2, 2), activation='relu'))
+            model.add(MaxPooling2D(pool_size=(2, 2)))
+            model.add(Flatten())
+            model.add(Dense(64, activation='relu'))
             model.add(Dense(128, activation='relu'))
             model.add(Dropout(0.4))
             model.add(Dense(self.num_classes, activation='softmax'))
